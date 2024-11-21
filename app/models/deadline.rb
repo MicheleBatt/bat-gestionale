3ecf14bd15f8445989a04f0285e7dc83aae5172e
MicheleBatt <michelebattistelli92@gmail.com>
2024-11-21 08:52:05 +0100

    fix model deadline

--- app/models/deadline.rb
+++ app/models/deadline.rb
@@ -7,7 +7,7 @@ class Deadline < ApplicationRecord
   # Callbacks
   before_validation { self.year = self.expired_at.year.to_i if self.expired_at }
   before_validation { self.month = self.expired_at.month.to_i if self.expired_at }
-  before_save { self.description = self.description.to_s.strip.capitalize if self.description }
+  before_save { self.description = self.description.to_s.strip if self.description }
 
 
   # Instance Methods
